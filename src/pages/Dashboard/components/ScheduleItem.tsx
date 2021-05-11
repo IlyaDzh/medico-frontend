fa798ee939255cec0e95ec910b5c073c9cc68140
Илья Долженко <ilay00@mail.ru>
2021-05-11 19:47:19 +0300

    fixed schedule item

--- src/pages/Dashboard/components/ScheduleItem.tsx
+++ src/pages/Dashboard/components/ScheduleItem.tsx
@@ -1,9 +1,7 @@
 import React from "react";
 import clsx from "clsx";
 import {
-    FormControlLabel,
     FormControl,
-    FormGroup,
     FormLabel,
     MenuItem,
     makeStyles,
@@ -30,6 +28,15 @@ const useStyles = makeStyles((theme: Theme) => ({
             marginBottom: 20
         }
     },
+    formInsideControl: {
+        display: "flex",
+        alignItems: "center",
+        flexDirection: "row",
+        marginBottom: 8,
+        "&:last-child": {
+            marginBottom: 0
+        }
+    },
     formLabel: {
         color: theme.palette.text.secondary,
         fontSize: 16,
@@ -39,18 +46,10 @@ const useStyles = makeStyles((theme: Theme) => ({
     formLabelWeekend: {
         color: `${theme.palette.primary.dark} !important`
     },
-    label: {
-        marginRight: 8,
-        width: 28
-    },
-    labelPlacementStart: {
-        width: "100%",
-        marginLeft: 0,
-        marginRight: 0,
-        marginBottom: 8,
-        "&:last-child": {
-            marginBottom: 0
-        }
+    selectLabel: {
+        width: 28,
+        fontSize: 18,
+        marginRight: 8
     },
     select: {
         width: "100%"
@@ -71,58 +70,46 @@ export const ScheduleItem: React.FC<IScheduleItem> = ({ title, isWeekend }) => {
             >
                 {title}
             </FormLabel>
-            <FormGroup>
-                <FormControlLabel
-                    classes={{
-                        label: classes.label,
-                        labelPlacementStart: classes.labelPlacementStart
-                    }}
-                    control={
-                        <Select className={classes.select} value="10">
-                            <MenuItem value="8">8:00</MenuItem>
-                            <MenuItem value="9">9:00</MenuItem>
-                            <MenuItem value="10">10:00</MenuItem>
-                            <MenuItem value="11">11:00</MenuItem>
-                            <MenuItem value="12">12:00</MenuItem>
-                            <MenuItem value="13">13:00</MenuItem>
-                            <MenuItem value="14">14:00</MenuItem>
-                            <MenuItem value="15">15:00</MenuItem>
-                            <MenuItem value="16">16:00</MenuItem>
-                            <MenuItem value="17">17:00</MenuItem>
-                            <MenuItem value="18">18:00</MenuItem>
-                            <MenuItem value="19">19:00</MenuItem>
-                            <MenuItem value="20">20:00</MenuItem>
-                        </Select>
-                    }
-                    label="с"
-                    labelPlacement="start"
-                />
-                <FormControlLabel
-                    classes={{
-                        label: classes.label,
-                        labelPlacementStart: classes.labelPlacementStart
-                    }}
-                    control={
-                        <Select className={classes.select} value="13">
-                            <MenuItem value="8">8:00</MenuItem>
-                            <MenuItem value="9">9:00</MenuItem>
-                            <MenuItem value="10">10:00</MenuItem>
-                            <MenuItem value="11">11:00</MenuItem>
-                            <MenuItem value="12">12:00</MenuItem>
-                            <MenuItem value="13">13:00</MenuItem>
-                            <MenuItem value="14">14:00</MenuItem>
-                            <MenuItem value="15">15:00</MenuItem>
-                            <MenuItem value="16">16:00</MenuItem>
-                            <MenuItem value="17">17:00</MenuItem>
-                            <MenuItem value="18">18:00</MenuItem>
-                            <MenuItem value="19">19:00</MenuItem>
-                            <MenuItem value="20">20:00</MenuItem>
-                        </Select>
-                    }
-                    label="по"
-                    labelPlacement="start"
-                />
-            </FormGroup>
+            <FormControl className={classes.formInsideControl}>
+                <label className={classes.selectLabel} htmlFor="from-time">
+                    с
+                </label>
+                <Select labelId="from-time" className={classes.select} value="10">
+                    <MenuItem value="8">8:00</MenuItem>
+                    <MenuItem value="9">9:00</MenuItem>
+                    <MenuItem value="10">10:00</MenuItem>
+                    <MenuItem value="11">11:00</MenuItem>
+                    <MenuItem value="12">12:00</MenuItem>
+                    <MenuItem value="13">13:00</MenuItem>
+                    <MenuItem value="14">14:00</MenuItem>
+                    <MenuItem value="15">15:00</MenuItem>
+                    <MenuItem value="16">16:00</MenuItem>
+                    <MenuItem value="17">17:00</MenuItem>
+                    <MenuItem value="18">18:00</MenuItem>
+                    <MenuItem value="19">19:00</MenuItem>
+                    <MenuItem value="20">20:00</MenuItem>
+                </Select>
+            </FormControl>
+            <FormControl className={classes.formInsideControl}>
+                <label className={classes.selectLabel} htmlFor="to-time">
+                    по
+                </label>
+                <Select labelId="to-time" className={classes.select} value="13">
+                    <MenuItem value="8">8:00</MenuItem>
+                    <MenuItem value="9">9:00</MenuItem>
+                    <MenuItem value="10">10:00</MenuItem>
+                    <MenuItem value="11">11:00</MenuItem>
+                    <MenuItem value="12">12:00</MenuItem>
+                    <MenuItem value="13">13:00</MenuItem>
+                    <MenuItem value="14">14:00</MenuItem>
+                    <MenuItem value="15">15:00</MenuItem>
+                    <MenuItem value="16">16:00</MenuItem>
+                    <MenuItem value="17">17:00</MenuItem>
+                    <MenuItem value="18">18:00</MenuItem>
+                    <MenuItem value="19">19:00</MenuItem>
+                    <MenuItem value="20">20:00</MenuItem>
+                </Select>
+            </FormControl>
         </FormControl>
     );
 };
